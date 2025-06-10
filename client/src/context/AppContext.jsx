8fcbf181968a44e24b1c039562edbca99a6a93ff
MeetAntala45 <meetantala36@gmail.com>
2025-06-10 10:46:17 +0530

    Course rating star fixed

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,32 +2,45 @@ import { useState, createContext, useEffect } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
+  const currency = import.meta.env.VITE_CURRENCY;
+  const navigate = useNavigate();
 
-    const currency = import.meta.env.VITE_CURRENCY
-    const navigate =useNavigate()
+  const [allCourses, setAllCourses] = useState([]);
 
-    const [allCourses,setAllCourses] = useState([])
+  //fetch all courses
 
-    //fetch all courses
+  const fetchAllCourses = async () => {
+    setAllCourses(dummyCourses);
+  };
 
-    const fetchAllCourses = async ()=>{
-        setAllCourses(dummyCourses)
+  //Function to calculate average rating of course
+
+  const calculateRating = (course) => {
+    if (course.courseRatings.length === 0) {
+      return 0;
     }
+    let totalRating = 0;
+    course.courseRatings.forEach((rating) => {
+      totalRating += rating.rating;
+    });
+    return totalRating / course.courseRatings.length;
+  };
 
-    useEffect(()=>{
-        fetchAllCourses()
-    },[])
-    const value = {
-        currency, allCourses, navigate
-    }
+  useEffect(() => {
+    fetchAllCourses();
+  }, []);
 
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
+  const value = {
+    currency,
+    allCourses,
+    navigate,
+    calculateRating,
+  };
 
-        </AppContext.Provider>
-    )
-}
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
