eb902490caebdc2d7255a7713da202158cb23370
MeetAntala45 <meetantala36@gmail.com>
2025-06-10 12:20:04 +0530

    Courselist page added

--- client/src/components/student/CoursesSection.jsx
+++ client/src/components/student/CoursesSection.jsx
@@ -4,8 +4,7 @@ import { AppContext } from "../../context/AppContext";
 import CourseCard from "./CourseCard";
 
 const CoursesSection = () => {
-
-  const {allCourses} = useContext(AppContext)
+  const { allCourses } = useContext(AppContext);
   return (
     <div className="py-16 md:px-40 px-8">
       <h2 className="text-3xl font-medium text-gray-800">
@@ -13,11 +12,13 @@ const CoursesSection = () => {
       </h2>
       <p className="text-sm md:text-base text-gray-500 mt-3">
         Discover our top-rated courses across various categories. From coding
-        and design to <br/> business and wellness, our courses are crafted to deliver
-        results.
+        and design to <br /> business and wellness, our courses are crafted to
+        deliver results.
       </p>
-      <div className="grid grid-cols-auto px-4 md:px-0 md:my-16 my-10 gap-4" >
-        {allCourses.slice(0,4).map((course,index)=><CourseCard key={index} course={course} />)}
+      <div className="grid grid-cols-auto px-4 md:px-0 md:my-16 my-10 gap-4">
+        {allCourses.slice(0, 4).map((course, index) => (
+          <CourseCard key={index} course={course} />
+        ))}
       </div>
       <Link
         to="/course-list"
